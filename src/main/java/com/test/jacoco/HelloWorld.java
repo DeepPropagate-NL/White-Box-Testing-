aa1145965c0071cfb32e949e1df228f151a6b024
superZh <zhouhang@superZhdeMacBook-Pro.local>
2019-11-15 21:33:38 +0800

    source file

---
+++ src/main/java/com/test/jacoco/HelloWorld.java
@@ -0,0 +1,74 @@
+package com.test.jacoco;
+
+import javax.imageio.event.IIOReadWarningListener;
+
+
+public class HelloWorld {
+    public HelloWorld() {
+
+    }
+
+    public String Method1() {
+        return "Hello World";
+    }
+
+    public int Method2(int a, int b) {
+        return a + b;
+    }
+    
+    public int Method3(int a, int b, int c){
+    	if((a>5&&b<0)||c>0) {
+    		System.out.println("Condition 1");
+    	}
+    	else if(a<5 && c<-2 ) {
+    		System.out.println("Condition 2");
+    	}
+    	else {
+    		System.out.println("Condition 3");
+    	}
+    	return 0;
+    }
+    public int Method4(int a, int b, int c, int d, float e) {
+    	if(a == 0) {
+    		return 0;
+    	}
+    	int x = 0;
+    	if((a == b) || ((c == d) && (bug(a)))) {
+    		 x = 1; 
+    	}
+    	e = 1/x;
+    	return 0;
+    }
+    public boolean bug(int a) {
+    	if(a == 5) return true;
+    	return false;
+    }
+    
+    
+    public boolean isTriangle(int a, int b, int c) {
+    	/**
+    	 * TODO: You need to complete this method to determine whether  a
+    	 * triangle is formed or not when given the input edge a, b and c.
+    	 */
+    	return true;
+    }
+    public boolean isBirthday(int year, int month, int day) {
+    	/**
+    	 * TODO: You need to complete this method to determine whether a 
+    	 * legitimate date of birth between 1990/01/01 and 2019/10/01 is 
+    	 * formed or not when given the input year, month and day.
+    	 */
+    	return true;
+    	
+    }
+    public Double miniCalculator(double a, double b, char op) {
+    	/**
+    	 * TODO: You need to complete this method to form a small calculator which 
+    	 * can calculate the formula: "a op b", the op here can be four basic  
+    	 * operation: "+","-","*","/". 
+    	 */
+    	double result = 0.0;
+    	return result;
+    }
+    
+}
---
+++ src/test/java/com/test/jacoco/test/HelloWorldTest.java
@@ -0,0 +1,39 @@
+package com.test.jacoco.test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by superZh on 2019/10/1.
+ */
+
+import org.junit.Test;
+import com.test.jacoco.HelloWorld;
+
+public class HelloWorldTest {
+    @Test
+    public void testMethod1() {
+        HelloWorld hw = new HelloWorld();
+        String a = hw.Method1();
+        assertEquals("Hello World", a);
+    }
+
+    @Test
+    public void testMethod2() {
+        HelloWorld hw = new HelloWorld();
+        int a = hw.Method2(2, 1);
+        assertEquals(3, a);
+    }
+    
+    @Test
+    public void testMethod3() {
+    	/**
+    	 * TODO：finish the test function
+    	 */
+    	HelloWorld hw = new HelloWorld();
+    	int a = hw.Method3(6, -1, 1);
+    	
+    }
+    /**
+     * TODO: add the test function of other methods in HelloWorld.java
+     */
+}
